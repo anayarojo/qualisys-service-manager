7f335ebcd2a2bb707c75099190d6dc2e3eadedf3
anayarojo <raulmartin_04@hotmail.com>
2018-02-23 09:53:58 -0700

    Main form size fix

--- QualisysServiceManager/frmManager.Designer.cs
+++ QualisysServiceManager/frmManager.Designer.cs
@@ -137,6 +137,7 @@
             this.Controls.Add(this.pnlForm);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(279, 169);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(279, 169);
             this.Name = "frmManager";
